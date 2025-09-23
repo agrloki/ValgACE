aa5b88c919c96ae1350ae7ecd0352b560460c1d9
agrloki <locky76@gmail.com>
2025-09-23 11:40:51 +0300

    2

--- extras/ace.py
+++ extras/ace.py
@@ -664,8 +664,8 @@ class ValgAce:
                     self.pdwell(1.0)
                 self.gcode.run_script_from_command(f'ACE_PARK_TO_TOOLHEAD INDEX={tool}')
                 self.toolhead.wait_moves()
-                self.pdwell(10.0)
-                while not self._park_is_toolchange :
+                #self.pdwell(10.0)
+                while not self._park_in_progress :
                     gcmd.respond_info(f"Park in progress")
                     self.pdwell(1.0)
                 self.gcode.run_script_from_command(f'_ACE_POST_TOOLCHANGE FROM={was} TO={tool}')
