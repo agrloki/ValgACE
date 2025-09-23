f7b207557a101ae284ef2b3006956a561f2f8bfc
agrloki <locky76@gmail.com>
2025-09-23 11:14:06 +0300

    replace pause with check status

--- extras/ace.py
+++ extras/ace.py
@@ -663,9 +663,9 @@ class ValgAce:
                 while self._info['slots'][was]['status'] != 'ready':
                     self.pdwell(1.0)
                 self.gcode.run_script_from_command(f'ACE_PARK_TO_TOOLHEAD INDEX={tool}')
-                self.pdwell(15.0)
-                # while self._info['slots'][tool]['status'] != 'ready':
-                #     self.pdwell(1.0)
+                #self.pdwell(15.0)
+                while self._park_is_toolchange != True:
+                     self.pdwell(1.0)
                 self.gcode.run_script_from_command(f'_ACE_POST_TOOLCHANGE FROM={was} TO={tool}')
                 self.toolhead.wait_moves()
                 gcmd.respond_info(f"Tool changed from {was} to {tool}")
